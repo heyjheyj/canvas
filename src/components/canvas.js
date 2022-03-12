829587f3a752e3c6b0b8bf9f913b1530c26dece8
hyejeong <withoo0414@gmail.com>
2022-03-12 17:00:27 +0900

    Feat: tailwindcss 적용

--- src/components/canvas.js
+++ src/components/canvas.js
@@ -1,13 +1,28 @@
 import React, { useState, useRef, useEffect } from "react";
-import styled from "styled-components";
-import ItemInfo from "./iteminfo";
+import ItemInfo from "./Iteminfo";
 
-const Canvas = (props) => {
+const setItem = (infoBox) => {
+  window.localStorage.setItem("infoBox", JSON.stringify(infoBox));
+};
+
+const getItem = () => {
+  return JSON.parse(window.localStorage.getItem("infoBox"));
+};
+
+const getPos = (event) => {
+  return {
+    x: event.nativeEvent.offsetX,
+    y: event.nativeEvent.offsetY
+  };
+};
+
+export default function Canvas() {
   const [infoBoxes, setInfoBoxes] = useState([]);
+  const [isEditingID, setIsEditingID] = useState(0);
   const [isDrawing, setIsDrawing] = useState(false);
 
-  const [offsetXY, setOffsetXY] = useState([]);
-  const [id, setId] = useState(1);
+  console.log(isEditingID);
+
   const [startXY, setStartXY] = useState([]);
   const [boxSize, setBoxSize] = useState({
     width: "",
@@ -18,35 +33,33 @@ const Canvas = (props) => {
   const [ctx, setCtx] = useState();
 
   useEffect(() => {
-    let result = canvasRef.current.getContext("2d");
-    setCtx(result);
-
-    let canvasOffset = canvasRef.current.getBoundingClientRect();
-    let resultoffestx = parseInt(canvasOffset.left);
-    let resultoffsety = parseInt(canvasOffset.top);
-    setOffsetXY([resultoffestx, resultoffsety]);
-  }, [canvasRef]);
+    if (canvasRef.current) {
+      let result = canvasRef.current.getContext("2d");
+      setCtx(result);
+    }
+  }, []);
 
   const startDrawing = (e) => {
     e.preventDefault();
-
+    if (isEditingID) {
+      return;
+    }
     setIsDrawing(true);
+    const pos = getPos(e);
+    const startX = pos.x;
+    const startY = pos.y;
 
-    console.log(offsetXY);
-    console.log(e.clientX, e.clientY);
-    let resultx = parseInt(e.clientX - offsetXY[0]);
-    let resulty = parseInt(e.clientY - offsetXY[1]);
-    console.log(resultx, resulty);
-    setStartXY([resultx, resulty]);
+    setStartXY([startX, startY]);
   };
 
   const drawing = (e) => {
     e.preventDefault();
-    if (!isDrawing) {
+    if (!isDrawing || isEditingID) {
       return;
     } else {
-      let mouseX = parseInt(e.clientX - offsetXY[0]);
-      let mouseY = parseInt(e.clientY - offsetXY[1]);
+      const pos = getPos(e);
+      const nowX = pos.x;
+      const nowY = pos.y;
 
       ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
       ctx.strokeStyle = "rgba(244,204,204, 0.5)";
@@ -54,8 +67,8 @@ const Canvas = (props) => {
       ctx.fillStyle = "rgba(244,204,204, 0.3)";
       ctx.fill();
 
-      let width = parseInt(mouseX - startXY[0]);
-      let height = parseInt(mouseY - startXY[1]);
+      const width = nowX - startXY[0];
+      const height = nowY - startXY[1];
       ctx.strokeRect(startXY[0], startXY[1], width, height);
       ctx.fillRect(startXY[0], startXY[1], width, height);
       setBoxSize({ width, height });
@@ -64,8 +77,11 @@ const Canvas = (props) => {
 
   const finishDrawing = (e) => {
     e.preventDefault();
+    if (isEditingID) {
+      return;
+    }
 
-    if (boxSize.width < 30 || boxSize.height < 30) {
+    if (Math.abs(boxSize.width) < 30 || Math.abs(boxSize.height) < 30) {
       alert("가로, 세로 길이는 최소 30px 이상이어야 합니다.");
       setIsDrawing(false);
       ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
@@ -75,7 +91,6 @@ const Canvas = (props) => {
     setIsDrawing(false);
     ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
     let text = prompt("영역의 이름은 무엇인가요?");
-    console.log(text);
 
     if (
       startXY[0] === undefined ||
@@ -86,89 +101,129 @@ const Canvas = (props) => {
       return;
     }
 
+    const endXY = [...startXY];
+    if (boxSize.width < 0) {
+      endXY[0] = startXY[0] - Math.abs(boxSize.width);
+    }
+    if (boxSize.height < 0) {
+      endXY[1] = startXY[1] - Math.abs(boxSize.height);
+    }
+
+    const id = Date.now();
     let newInfobox = {
       id,
-      startXY,
-      boxSize,
+      startXY: endXY,
+      boxSize: {
+        width: Math.abs(boxSize.width),
+        height: Math.abs(boxSize.height)
+      },
       zindex: id,
       text
     };
 
-    setInfoBoxes([...infoBoxes, newInfobox]);
-    setId((prev) => prev + 1);
+    const newInfoBoxes = [...infoBoxes, newInfobox];
+    setInfoBoxes(newInfoBoxes);
+    setItem(newInfoBoxes);
   };
 
-  const deleteItemInfo = (selectedItem) =>
-    setInfoBoxes(infoBoxes.filter((item) => item.id !== selectedItem.id));
+  const deleteItemInfo = (selectedItem) => {
+    const newInfoBoxes = infoBoxes.filter(
+      (item) => item.id !== selectedItem.id
+    );
+    setInfoBoxes(newInfoBoxes);
+    setItem(newInfoBoxes);
+  };
+
+  const onChangeText = (text, selectedItem) => {
+    // console.log(text, selectedItem);
+    let result = [];
+    for (let i = 0; i < infoBoxes.length; i++) {
+      // console.log(infoBoxes[i]);
+      if (infoBoxes[i].id === selectedItem.id) {
+        infoBoxes[i].text = text;
+        result.push(infoBoxes[i]);
+      } else {
+        result.push(infoBoxes[i]);
+      }
+    }
+    setInfoBoxes(result);
+    setItem(result);
+  };
+
+  const changeCoordiante = (newCoordinate, item) => {
+    if (newCoordinate.width < 30 || newCoordinate.height < 30) {
+      setInfoBoxes([...infoBoxes]);
+      setItem([...infoBoxes]);
+    } else {
+      let result = [];
+      for (let i = 0; i < infoBoxes.length; i++) {
+        if (infoBoxes[i].id === item.id) {
+          infoBoxes[i].boxSize.width = newCoordinate.width;
+          infoBoxes[i].boxSize.height = newCoordinate.height;
+          result.push(infoBoxes[i]);
+        } else {
+          result.push(infoBoxes[i]);
+        }
+      }
+      setInfoBoxes(result);
+      setItem(result);
+    }
+  };
+
+  useEffect(() => {
+    const newInfoBoxes = getItem();
+    if (newInfoBoxes !== null) setInfoBoxes(newInfoBoxes);
+  }, []);
 
   return (
     <>
-      <CanvasComponent>
-        <ShowItems>
-          {infoBoxes &&
-            infoBoxes?.map((item, index) => <li key={index}>{item.text}</li>)}
-        </ShowItems>
-        <InfoBoxContainer>
-          {infoBoxes &&
-            infoBoxes?.map((item, index) => (
-              <ItemInfo key={index} item={item} onDelete={deleteItemInfo} />
-            ))}
-        </InfoBoxContainer>
-        <DrawArea
-          ref={canvasRef}
-          width={700}
-          height={874}
-          onMouseDown={startDrawing}
-          onMouseMove={drawing}
-          onMouseUp={finishDrawing}
-        ></DrawArea>
-      </CanvasComponent>
+      <div className="flex flex-col items-center justify-center w-full min-h-screen">
+        <div className="w-2/3 m-auto">
+          <span style={{ display: "block" }}>
+            1. 각 상품정보 박스 Title은 클릭해서 수정하고 Enter를 누르면 수정이
+            반영됩니다.
+          </span>
+          <span style={{ display: "block" }}>
+            2. 각 상품정보 박스 오른쪽 하단 모서리를 드래그해 크기를 조정할 수
+            있습니다.
+          </span>
+        </div>
+        <div className="relative">
+          <div
+            className="absolute z-50 h-auto font-semibold list-outside bg-white w-52 bg-opacity-70 top-3 left-3 p-7"
+            style={{ listStyleType: "square" }}
+          >
+            {infoBoxes &&
+              infoBoxes?.map((item, index) => <li key={index}>{item.text}</li>)}
+          </div>
+          <div className="absolute top-0 left-0 z-10 w-0 h-0 space-y-1 break-all">
+            {infoBoxes &&
+              infoBoxes?.map((item, index) => (
+                <ItemInfo
+                  key={index}
+                  item={item}
+                  onDelete={deleteItemInfo}
+                  onChangeText={onChangeText}
+                  onChangeCoordinate={changeCoordiante}
+                  isEditingID={isEditingID}
+                  setIsEditingID={setIsEditingID}
+                />
+              ))}
+          </div>
+          <canvas
+            ref={canvasRef}
+            onMouseDown={startDrawing}
+            onMouseMove={drawing}
+            onMouseUp={finishDrawing}
+            width={700}
+            height={874}
+            style={{
+              backgroundImage: "url('/images/background.jpeg')",
+              backgroundSize: "700px 874px"
+            }}
+          />
+        </div>
+      </div>
     </>
   );
-};
-
-export default Canvas;
-
-const CanvasComponent = styled.div`
-  /* width: 100%;
-  height: 100%; */
-  /* display: flex;
-  justify-content: center;
-  align-items: center; */
-`;
-
-const DrawArea = styled.canvas`
-  width: 700px;
-  height: 874px;
-  top: 0;
-  background-image: url("/images/background.jpeg");
-  background-size: 700px 874px;
-  position: absolute;
-  z-index: 0;
-`;
-
-const InfoBoxContainer = styled.ul`
-  width: 700px;
-  height: 874px;
-  position: relative;
-`;
-
-const ShowItems = styled.ul`
-  width: 120px;
-  min-height: 50px;
-  height: auto;
-  background: #ffffff70;
-  z-index: 999;
-  top: 10px;
-  left: 10px;
-  position: absolute;
-  padding: 10px 0 30px 30px;
-  list-style-position: outside;
-  list-style-type: square;
-  font-weight: 600;
-  li {
-    padding-top: 5px;
-    max-height: 40px;
-    word-break: break-all;
-  }
-`;
+}
--- src/components/iteminfo.js
+++ src/components/iteminfo.js
@@ -1,59 +1,93 @@
-import React from "react";
-import styled from "styled-components";
+import React, { useRef } from "react";
 
-const ItemInfo = ({ item, onDelete }) => {
-  // console.log("[ItemInfo]item:", item);
+export default function ItemInfo({
+  item,
+  onDelete,
+  onChangeText,
+  onChangeCoordinate,
+  isEditingID,
+  setIsEditingID
+}) {
+  const boxRef = useRef();
+  const changeText = (e) => {
+    if (e.keyCode === 13) {
+      onChangeText(e.target.value, item);
+      boxRef.current.style.resize = "both";
+      setIsEditingID(0);
+    }
+  };
+
+  const onBlur = (e) => {
+    onChangeText(e.target.value, item);
+    boxRef.current.style.resize = "both";
+    setIsEditingID(0);
+  };
+
+  const coordiante = (nativeEvent, e) => {
+    e.stopPropagation();
+    if (e.target.nodeName === "SPAN" || isEditingID) {
+      return;
+    }
+    let coordinate = {
+      width: nativeEvent.layerX,
+      height: nativeEvent.layerY
+    };
+    onChangeCoordinate(coordinate, item);
+  };
+
+  const startChangeCoordinate = (e) => {
+    e.stopPropagation();
+    if (isEditingID || e.target.nodeName === "SPAN") {
+      boxRef.current.style.resize = "none";
+    }
+  };
+
   return (
-    <Info
-      top={item.startXY[1]}
-      left={item.startXY[0]}
-      width={item.boxSize.width}
-      height={item.boxSize.height}
-      zindex={item.zindex}
+    <li
+      ref={boxRef}
+      onMouseDown={(e) => startChangeCoordinate(e)}
+      onMouseUp={(e) => coordiante(e.nativeEvent, e)}
+      style={{
+        top: `${item.startXY[1]}px`,
+        left: `${item.startXY[0]}px`,
+        width: `${item.boxSize.width}px`,
+        height: `${item.boxSize.height}px`,
+        borderColor: "#d9ead3",
+        backgroundColor: "#d9ead370",
+        resize: "both"
+      }}
+      className="absolute z-30 flex flex-row justify-between pl-1 overflow-auto break-all border-2"
     >
-      <span>{item.text}</span>
-      <DeleteButton onClick={() => onDelete(item)}>X</DeleteButton>
-    </Info>
+      {isEditingID === item.id ? (
+        <input
+          className="w-3/4 pl-1 mt-1 text-sm h-7"
+          style={{ backgroundColor: "rgba(244,204,204, 0.5)" }}
+          type="text"
+          defaultValue={item.text}
+          onKeyUp={(e) => changeText(e)}
+          onBlur={(e) => onBlur(e)}
+          autoFocus
+        />
+      ) : (
+        <span
+          className="w-full h-auto pt-1 pr-6 text-sm font-semibold"
+          onClick={() => {
+            console.log("click");
+            setIsEditingID(item.id);
+          }}
+        >
+          {item.text}
+        </span>
+      )}
+      <button
+        className="absolute w-5 h-5 border-0 cursor-pointer top-1 right-1 hover:scale-105"
+        style={{
+          backgroundColor: "#2f485850"
+        }}
+        onClick={() => onDelete(item)}
+      >
+        X
+      </button>
+    </li>
   );
-};
-
-const Info = styled.li`
-  top: ${(props) => props.top}px;
-  left: ${(props) => props.left}px;
-  width: ${(props) => props.width}px;
-  height: ${(props) => props.height}px;
-  z-index: ${(props) => props.zindex};
-  background: #d9ead370;
-  position: absolute;
-  padding-left: 5px;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  border: 2px solid #d9ead3;
-  word-break: break-all;
-  overflow: scroll;
-  &::-webkit-scrollbar {
-    display: none;
-  }
-  span {
-    padding-right: 25px;
-    font-size: 15px;
-    font-weight: 600;
-  }
-`;
-
-const DeleteButton = styled.button`
-  width: 20px;
-  height: 20px;
-  position: absolute;
-  right: 5px;
-  top: 2px;
-  border: none;
-  background: #2f485850;
-  cursor: pointer;
-  &:hover {
-    transform: scale(1.05);
-  }
-`;
-
-export default ItemInfo;
+}
